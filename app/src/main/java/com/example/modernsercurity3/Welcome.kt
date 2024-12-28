1e09a4f6f4c86a86f53e8b0e3a0beb16d1c48ff4
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-12-28 16:18:49 +0200

    kt

--- app/src/main/java/com/example/modernsercurity3/Welcome.kt
+++ app/src/main/java/com/example/modernsercurity3/Welcome.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class Welcome : Fragment() {
 
@@ -15,7 +17,10 @@ class Welcome : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_welcome, container, false)
 
-        val button = view.findViewById<Button>(R.id.)
+        val button = view.findViewById<Button>(R.id.get_started_button)
+        button.setOnClickListener{ view ->
+            findNavController().navigate(R.id.login)
+        }
 
         return view
     }
